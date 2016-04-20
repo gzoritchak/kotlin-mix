a57faae4fe755028247bf8900e3fe98d445071d9
gzoritchak <g.zoritchak@gmail.com>
2016-04-21 01:59:02 +0200

    Faker implementation.

--- src/main/java/org/mixit/kotlin/web/HotelsController.kt
+++ src/main/java/org/mixit/kotlin/web/HotelsController.kt
@@ -1,24 +1,43 @@
 package org.mixit.kotlin.web
 
+import com.github.javafaker.Faker
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.*
 
 @Controller
 class HotelsController {
 
+
+    data class Hotel(
+            val id:Long,
+            val name: String,
+            val address:String,
+            val zip:String,
+            val city:String,
+            val country:String = "France"
+    )
+
+    val faker = Faker()
+
+
     @RequestMapping("/api/hotels",method = arrayOf(RequestMethod.GET))
     @ResponseBody
-    fun getHotels(){
-        error("Not implemented")
-    }
+    fun getHotels() =
+            (1..33).map {
+                Hotel(it.toLong(),
+                        "Hotel ${faker.address().cityName()}",
+                        "${faker.address().streetAddress()}",
+                        "${faker.address().zipCode()}",
+                        "${faker.address().cityName()}")
+            }
+
+
+    data class ChangeNameCmd (val name: String)
 
     @RequestMapping("/api/hotels/{id}", method = arrayOf(RequestMethod.PUT))
     @ResponseBody
-    fun changeName(@PathVariable id:Long){
-        error("Not implemented")
+    fun changeName(@PathVariable id:Long, @RequestBody cmd:ChangeNameCmd){
+        println("Changing name of hotel $id :: $cmd")
     }
 
 }
